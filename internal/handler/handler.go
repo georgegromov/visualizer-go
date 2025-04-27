00444a60d815cbed86ecfcaab16ea218d064b78e
George Gromov <gromov.dev13@gmail.com>
2025-04-28 02:36:57 +0300

    add comments

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -28,27 +28,32 @@ func (h *Handler) Init() *gin.Engine {
 
 	handler.Use(gin.Recovery(), gin.Logger(), middlewares.CorsMiddleware(h.origin))
 
+	// define group route /api
 	api := handler.Group("/api")
 	{
+		// get /api/status
 		api.GET("/status", func(ctx *gin.Context) {
 			ctx.String(http.StatusOK, "ok")
 		})
 
+		// define group route /api/auth
 		auth := api.Group("/auth")
 		{
 			auth.POST("/login", h.login)
 		}
 
+		// define group route protected
 		protected := api.Group("")
 		protected.Use(middlewares.AuthMiddleware(h.log))
 		{
+			// define user group route /api/users
 			users := protected.Group("/users")
 			{
 				users.GET("/:id", h.getUserByID)
 				users.POST("", h.createUser)
 				users.PATCH("/:id", h.updateUser)
 			}
-
+			// define user group route /api/templates
 			templates := protected.Group("/templates")
 			{
 				templates.POST("", h.createTemplate)
@@ -57,17 +62,20 @@ func (h *Handler) Init() *gin.Engine {
 				templates.PATCH("/:id", h.updateTemplate)
 			}
 
-      // TODO: переделать в dashboards
+			// TODO: переделать в dashboards
+			// define user group route /api/visualizations
 			visualizations := protected.Group("/visualizations")
 			{
 				visualizations.POST("", h.createVisualization)
 				visualizations.GET("", h.getAllVisualizations)
-        // переделать в api/templates/{id}/dashboards
+				// переделать в api/templates/{id}/dashboards
 				visualizations.GET("/t/:id", h.getVisualizationsByTemplateID)
 				visualizations.GET("/:id", h.getVisualizationByID)
 				visualizations.PATCH("/:id", h.updateVisualization)
 				visualizations.DELETE("/:id", h.deleteVisualization)
 			}
+
+			// get /api/visualizations/share/:id
 			api.GET("/visualizations/share/:id", h.getVisualizationByShareID)
 		}
 	}

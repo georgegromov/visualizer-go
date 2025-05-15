4ec070e3e450427ac51fd5499229e970ea986465
GEORGE GROMOV <gromov.dev13@gmail.com>
2025-05-15 21:13:46 +0300

    fix

--- internal/models/chart.go
+++ internal/models/chart.go
@@ -4,14 +4,15 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/jmoiron/sqlx/types"
 )
 
 type Chart struct {
-	ID           uuid.UUID    `json:"id" db:"id"`
-	Name         *string      `json:"name" db:"name"`
-	Type         string       `json:"type" db:"type"`
-	Measurements *interface{} `json:"measurements" db:"measurements"`
-	CanvasID     *uuid.UUID   `json:"canvasId" db:"canvas_id"`
-	UpdatedAt    *time.Time   `json:"updatedAt" db:"updated_at"`
-	CreatedAt    time.Time    `json:"createdAt" db:"created_at"`
+	ID           uuid.UUID       `json:"id" db:"id"`
+	Name         *string         `json:"name" db:"name"`
+	Type         string          `json:"type" db:"type"`
+	Measurements *types.JSONText `json:"measurements" db:"measurements"`
+	CanvasID     *uuid.UUID      `json:"canvasId" db:"canvas_id"`
+	UpdatedAt    *time.Time      `json:"updatedAt" db:"updated_at"`
+	CreatedAt    time.Time       `json:"createdAt" db:"created_at"`
 }

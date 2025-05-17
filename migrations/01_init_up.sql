d93367dc44b0743543f0c948e913d5c8cf4871c7
GEORGE GROMOV <gromov.dev13@gmail.com>
2025-05-17 18:50:04 +0300

    fix

--- migrations/01_init_up.sql
+++ migrations/01_init_up.sql
@@ -1,55 +1,57 @@
 create table users (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    username VARCHAR(32) NOT NULL,
-    password_hash VARCHAR(255) NOT NULL,
-    role TEXT NOT NULL DEFAULT 'User' CHECK (role IN ('Admin', 'User', 'Customer')),
-    updated_at TIMESTAMPTZ,
-    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  username VARCHAR(32) NOT NULL,
+  password_hash VARCHAR(255) NOT NULL,
+  role TEXT NOT NULL DEFAULT 'User' CHECK (role IN ('Admin', 'User', 'Customer')),
+  updated_at TIMESTAMPTZ,
+  created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
 create table templates (
 	id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
 	name VARCHAR(64),
-    description VARCHAR(255),
-    is_deleted BOOLEAN DEFAULT false,
+  description VARCHAR(255),
+  is_deleted BOOLEAN DEFAULT false,
 	creator_id UUID,
 	
 	updated_at TIMESTAMPTZ,
-    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+  created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
 	
 	CONSTRAINT fk_template_creator FOREIGN KEY (creator_id) REFERENCES users(id)
 );
 create table canvases (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    name TEXT,
-    description TEXT,
-    template_id UUID NOT NULL,
-    updated_at TIMESTAMP,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    CONSTRAINT fk_canvas_template FOREIGN KEY (template_id) REFERENCES templates(id) ON DELETE CASCADE
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  name TEXT,
+  description TEXT,
+  template_id UUID NOT NULL,
+  updated_at TIMESTAMP,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  CONSTRAINT fk_canvas_template FOREIGN KEY (template_id) REFERENCES templates(id) ON DELETE CASCADE
 );
 create table charts (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    name TEXT,
-    type TEXT NOT NULL,
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  name TEXT,
+  type TEXT NOT NULL,
 	measurements jsonb,
-    canvas_id UUID NOT NULL,
-    updated_at TIMESTAMP,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    CONSTRAINT fk_chart_canvas FOREIGN KEY (canvas_id) REFERENCES canvases(id) ON DELETE CASCADE
+  canvas_id UUID NOT NULL,
+  udated_at TIMESTAMP,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  CONSTRAINT fk_chart_canvas FOREIGN KEY (canvas_id) REFERENCES canvases(id) ON DELETE CASCADE
 );
 create table dashboards (
 	id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
 	name VARCHAR(64) NOT NULL,
-    description VARCHAR(255),
-	creator_id UUID,
+  description VARCHAR(255),
+	is_publish BOOL NOT NULL DEFAULT false,
+	share_id UUID NOT NULL DEFAULT gen_random_uuid(),
+	creator_id UUID NOT NULL,
 	template_id UUID NOT NULL,
 	
 	view_count INTEGER DEFAULT 0,
 	
-    last_viewed_at TIMESTAMP,
+  last_viewed_at TIMESTAMP,
 	updated_at TIMESTAMPTZ,
-    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+  created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
 	
 	CONSTRAINT fk_dashboard_template FOREIGN KEY (template_id) REFERENCES templates(id),
-    CONSTRAINT fk_dashboard_creator FOREIGN KEY (creator_id) REFERENCES users(id)
+  CONSTRAINT fk_dashboard_creator FOREIGN KEY (creator_id) REFERENCES users(id)
 );
